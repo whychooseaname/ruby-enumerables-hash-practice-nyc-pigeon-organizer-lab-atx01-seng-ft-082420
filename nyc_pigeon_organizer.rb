cfd91e7248bf8d690dfbe1ce9bad43f32bca8ec0
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-09 04:43:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,7 +24,8 @@ end
   #iterate over the hash with color,gender, and lives, and the value of those keys
    #iterate over the values of the inner key, and the name values
     #iterate over the names of the pigeons with the value of the name
-      #if the name value is not empty array made at the beginning
+      #if the name value is not in the empty array made at the beginning
         #put it in the array
+        #if the name is the value of the color, gender, and lives keys
 
 #{:color=>{:purple=>["Theo", "Peter Jr.", "Lucky"], :grey=>["Theo", "Peter Jr.","Ms. K"], :white=>["Queenie", "Andrew", "Ms. K", "Alex"], :brown=>["Queenie", "Alex"]}, :gender=>{:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"], :female=>["Queenie", "Ms. K"]}, :lives=>{"Subway"=>["Theo", "Queenie"], "Central Park"=>["Alex", "Ms. K", "Lucky"], "Library"=>["Peter Jr."], "City Hall"=>["Andrew"]}}
