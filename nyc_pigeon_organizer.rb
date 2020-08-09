a284a4dfd5c555513fe7fcf5e0f08b20bdf5029c
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-09 05:05:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,12 +5,12 @@ def nyc_pigeon_organizer(data)
   nyc_pigeons={}
   data.each do |color_gender_lives, value|
     value.each do |stats, names|
-      names.each do |name_of_pigeons|
-        if nyc_pigeons[name_of_pigeons]==nil 
-          nyc_pigeons[name_of_pigeons]={}
+      names.each do |name_of_pigeon|
+        if nyc_pigeons[name_of_pigeon]==nil 
+          nyc_pigeons[name_of_pigeon]={}
         end
-        if nyc_pigeons[name_of_pigeons][color_gender_lives]=nil
-          nyc_pigeons[name_of_pigeons][color_gender_lives]=[]
+        if nyc_pigeons[name_of_pigeon][color_gender_lives]==nil
+          nyc_pigeons[name_of_pigeon][color_gender_lives]=[]
         end
       
       end
@@ -20,6 +20,7 @@ def nyc_pigeon_organizer(data)
   
   nyc_pigeons
 end
+
 
 #method nyc pigeon organizer (hash called data)
 # create an empty hash for list of pigeons with their name
