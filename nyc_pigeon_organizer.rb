edaa38ed91bdd06942ecf86a95c4f06edbfcb8c3
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-09 04:38:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,5 +19,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list
 end
 
+#method nyc pigeon organizer (hash called data)
+# create a list of pigeons with their name
+  #iterate over the hash with color,gender, and lives, and the value of those keys
+   #iterate over the values 
 
 #{:color=>{:purple=>["Theo", "Peter Jr.", "Lucky"], :grey=>["Theo", "Peter Jr.","Ms. K"], :white=>["Queenie", "Andrew", "Ms. K", "Alex"], :brown=>["Queenie", "Alex"]}, :gender=>{:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"], :female=>["Queenie", "Ms. K"]}, :lives=>{"Subway"=>["Theo", "Queenie"], "Central Park"=>["Alex", "Ms. K", "Lucky"], "Library"=>["Peter Jr."], "City Hall"=>["Andrew"]}}
