aaa3b8d16fc5b1f5359dcf6e2b3a9bb9a088926d
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-09 05:10:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,8 @@ def nyc_pigeon_organizer(data)
         if nyc_pigeons[name_of_pigeon][color_gender_lives]==nil
           nyc_pigeons[name_of_pigeon][color_gender_lives]=[]
         end
-        if nyc_pigeons[name_of_pigeon][color_gender_lives][stats]=[]
+        if nyc_pigeons[name_of_pigeon][color_gender_lives][stats]==nil
+          nyc_pigeons[name_of_pigeon][color_gender_lives][stats]=[]
       end
     end
   end 
